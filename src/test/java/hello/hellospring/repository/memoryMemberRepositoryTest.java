1ffd9721c2e15365c66e70b8913eb65634b30332
jang <wkdwnsrl13@naver.com>
2023-10-19 00:47:05 +0900

    Initial commit

---
+++ src/test/java/hello/hellospring/repository/memoryMemberRepositoryTest.java
@@ -0,0 +1,19 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.service.MemberService;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Member;
+
+
+class memoryMemberRepositoryTest {
+    MemberService memberService;
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @Test
+    public void save(){
+
+        }
+    }
+
+
---
+++ src/test/java/hello/hellospring/service/memberServiceTest.java
@@ -0,0 +1,13 @@
+package hello.hellospring.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class memberServiceTest {
+
+    @Test
+    void 회원가입() {
+
+    }
+}
