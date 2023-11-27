daa4d922ff5f81c7a85cac97896c7d8e50d74b91
JANGJUNKI\wkdwn <wkdwnsrl123@gmail.com>
2023-11-27 15:11:02 +0900

    스웨거 적용, https 주석

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,17 +4,34 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 //import io.swagger.v3.oas.annotations.Hidden;
 //import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-//@Tag(name = "예제 API", description = "Swagger 테스트용 API")
+
+/*@Api → @Tag
+@ApiIgnore → @Parameter(hidden = true) or @Operation(hidden = true) or @Hidden
+@ApiImplicitParam → @Parameter
+@ApiImplicitParams → @Parameters
+@ApiModel → @Schema
+@ApiModelProperty(hidden = true) → @Schema(accessMode = READ_ONLY)
+@ApiModelProperty → @Schema
+@ApiOperation(value = "foo", notes = "bar") → @Operation(summary = "foo", description = "bar")
+@ApiParam → @Parameter
+@ApiResponse(code = 404, message = "foo") → @ApiResponse(responseCode = "404", description = "foo")*/
+@Tag(name = "예제 API", description = "Swagger 테스트용 API")
 @RestController
 @RequestMapping("/api/members")
 public class MemberController {
     @Autowired
     private MemberService memberService;
 
+
+    @Operation(summary = "멤버조회1", description = "멤버조회2")
+    @Parameter(name = "멤버조회3", description = "멤버조회4")
     @GetMapping
     public List<Member> getAllMembers() {
         return memberService.getAllMembers();
